402bd37ec0ffbe8467099f16fee3b325e9cafd43
Mohd Arsalan Khan <arsalan787khan@gmail.com>
2024-11-10 20:15:55 +0530

    add comments section to the admin dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import verifyToken from "../middlewares/verify.js";
-import { createComment, deleteComment, editComment, getPostComment, likeComment } from "../controllers/comment.controller.js";
+import { createComment, deleteComment, editComment, getComments, getPostComment, likeComment } from "../controllers/comment.controller.js";
 
 const router = Router()
 
@@ -9,5 +9,6 @@ router.route('/getpostcomments/:postId').get(getPostComment)
 router.route('/likeComment/:commentId').put(verifyToken, likeComment)
 router.route('/editComment/:commentId').put(verifyToken, editComment)
 router.route('/deleteComment/:commentId').delete(verifyToken, deleteComment)
+router.route('/getcomments').get(verifyToken, getComments)
 
 export default router
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashSidebar from "../components/DashSidebar"
 import DashProfile from "../components/DashProfile"
 import DashPosts from '../components/DashPosts'
 import DashUsers from '../components/DashUsers'
+import DashComment from '../components/DashComment'
 
 function Dashboard() {
   const location = useLocation()
@@ -30,6 +31,8 @@ function Dashboard() {
       {tab === "posts" && <DashPosts/>}
       {/* users */}
       {tab === "users" && <DashUsers/>}
+      {/* comment */}
+      {tab === "comments" && <DashComment/>}
     </div>
   )
 }
